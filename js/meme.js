1686a57e2c36a9c04445f364f74bce4530610522
Bossmails <awotundenelson321@gmail.com>
2026-02-17 13:18:40 +0100

    Delete js directory

--- js/meme.js
+++
@@ -1,8 +0,0 @@
-function updateMeme() {
-  const img = document.getElementById("image").value;
-  document.getElementById("memeImg").src = "images/" + img;
-}
-
-function updateText(text) {
-  document.getElementById("memeText").innerText = text;
-}
